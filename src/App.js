0210474cc342ae70b1b64a6c61d753ea59576e4b
Francisco Orsillo <fran_orsillo@hotmail.com>
2023-02-25 23:33:30 -0300

    Navbar Logo estilos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Footer } from "./Components/Footer/Footer";
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
 import { Navbar } from "./Components/Navbar/Navbar";
+import ProductCard from "./Components/ProductCard/ProductCard";
 
 function App() {
   return (
     <div>
       <Navbar />
       <ItemListContainer />
+      <ProductCard />
       <Footer />
     </div>
   );
--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,9 +1,14 @@
-const Logo = () => {
+/* const Logo = () => {
   return (
     <div>
-      <h4>Logo</h4>
+      ,
+      <img
+        src="https://res.cloudinary.com/drftutaye/image/upload/v1677377994/Logo_dvssna.png"
+        alt="Logo Bass Center"
+      />
     </div>
   );
 };
 
 export default Logo;
+ */
